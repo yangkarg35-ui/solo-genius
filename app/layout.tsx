e98c7f744290a7ebe45144e219e4e745d1f3ee2c
macbook air <macbookair@macbooks-MacBook-Air.local>
2026-08-13 18:57:51 +0630

    Fix layout import path to relative SmoothScroll in app folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import SmoothScroll from "@/components/SmoothScroll";
+import SmoothScroll from "./SmoothScroll";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
